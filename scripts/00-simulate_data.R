c36b1c249e6ed1e85b85923bb176cd13df619c0f
Wendy Huang <yanfei.huang@mail.utoronto.ca>
2024-11-16 20:37:09 -0500

    simulated

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -11,41 +11,79 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(dplyr)
 set.seed(853)
 
 
-#### Simulate data ####
-# Define Country Name
-country <- c(
-  "Canada",
-  "Australia",
-  "Argentina",
-  "Brazil",
-  "United States of America",
-  "Portugal",
-  "Qatar",
-  "Republic of Korea"
-)
+#### Define data columns ####
+# Set numbers of rows in simulated data
+n<-50
 
-# Political parties
-parties <- c("Labor", "Liberal", "Greens", "National", "Other")
-
-# Create a dataset by randomly assigning states and parties to divisions
-analysis_data <- tibble(
-  division = paste("Division", 1:151),  # Add "Division" to make it a character
-  state = sample(
-    states,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.25, 0.25, 0.15, 0.1, 0.1, 0.1, 0.025, 0.025) # Rough state population distribution
-  ),
-  party = sample(
-    parties,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.40, 0.40, 0.05, 0.1, 0.05) # Rough party distribution
+# Create a mapping of countries to status
+country_status_map <- tibble(
+  country = c(
+  "Canada", "Australia", "Argentina", "Brazil", "United States of America", "Portugal", "Qatar", "Republic of Korea"),
+  status = c(
+    'Developing', 'Developed', 'Developing', 'Developing', 'Developed', 'Developed', 'Developing', 'Developing')
   )
+
+# Define Year 
+year <- 2000:2015
+
+
+#### Simulate data
+simulated_expectancy <- tibble(
+  #randomly select countries from the 10 countries above
+  country = as.character(sample(country_status_map, n, replace = TRUE)) )%>%
+  
+  # Add the fixed status based on the country
+  left_join(country_status_map, by = "country") %>%  
+  
+  mutate(
+  #randomly select years from 2000 to 2015
+  year = sample(year, n, replace = TRUE),
+  
+  life_expectancy = ifelse(status == "Developed",
+                           #for developed countries, draw normal distribution with mean 80 and standard deviation of 5
+                           rnorm(n, mean = 80, sd = 5),
+                           #for developing countries, draw normal distribution with mean 65 and standard deviation of 10
+                           rnorm(n, mean = 65, sd = 10)),
+  
+  #using a normal distribution with a mean of 25 and a standard deviation of 5 for all observations of BMI
+  BMI = rnorm(n, mean = 25, sd = 5),
+  
+  
+  GDP = ifelse(status == "Developed",
+               #for developed countries, draw normal distribution with mean 40,000 and standard deviation of 10,000
+               rnorm(n, mean = 40000, sd = 10000),
+               #for developing countries, draw normal distribution with mean 5,000 and standard deviation of 2,000
+               rnorm(n, mean = 5000, sd = 2000)),
+  
+  #random value between 1 to 100,000 of Expenditure on health as a percentage of Gross Domestic Product per capita(%)
+  percentage_expenditure = runif(n, min = 1, max = 100000),
+  
+  #uniform distribution of value of general government expenditure as a  between 1 and 20
+  total_expenditure = runif(n, min = 1, max = 20),
+  
+  #uniform distribution of random values of human development index of income composition between 0.3 and 0.9
+  income_composition_resources = runif(n, min = 0.3, max = 0.9),
+  
+  schooling = ifelse(status == "Developed",
+                     #for developed countries, draw normal distribution with mean 15 and standard deviation of 2
+                     rnorm(n, mean = 15, sd = 2),
+                     #for developing countries, draw normal distribution with mean 8 and standard deviation of 3
+                     rnorm(n, mean = 8, sd = 3))
 )
+
+# Ensure no negative values exist for life expectancy, BMI, GDP or Schooling
+simulated_expectancy <- simulated_expectancy %>%
+  mutate(
+    life_expectancy = ifelse(life_expectancy < 0, 0, life_expectancy),
+    BMI = ifelse(BMI < 0, 0, BMI),
+    GDP = ifelse(GDP < 0, 0, GDP),
+    schooling = ifelse(schooling < 0, 0, schooling)
+  )
+
 
 
 #### Save data ####
