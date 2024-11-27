fb8160327084f7654dd573dc16fc38466bc48bc6
Wendy Huang <yanfei.huang@mail.utoronto.ca>
2024-11-26 22:05:46 -0500

    data cleaning

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -8,18 +8,22 @@
 # Any other information needed? No
 
 #### Workspace setup ####
+install.packages("countrycode")
 library(tidyverse)
 library(dplyr)
 library(janitor)
 library(arrow)
+library(countrycode)
 
 #### Clean data ####
 #Load the dataset
 raw_expectancy <- read_csv("data/01-raw_data/raw_data.csv")
 
 # Data cleaning by Filter and select relevant columns, renaming complex names, 
-# filtering invalid entries, 
-cleaned_expectancy <-raw_expectancy |> 
+# filtering invalid entries, round columns into specific decimals 
+cleaned_expectancy <- tryCatch(
+  {
+    raw_expectancy |> 
   select(Country, Year, Status, Life.expectancy, percentage.expenditure, BMI, Total.expenditure, GDP,
          Income.composition.of.resources, Schooling) |>
   rename(LifeExpectancy = Life.expectancy,
@@ -27,6 +31,7 @@ cleaned_expectancy <-raw_expectancy |>
          TotalExpenditure = Total.expenditure,
          IncomeComposition = Income.composition.of.resources) |>
   filter (
+    # filtering invalid entries 
     !is.na(GDP), 
     !is.na(Year),
     !is.na(Status),
@@ -35,7 +40,29 @@ cleaned_expectancy <-raw_expectancy |>
     !is.na(BMI),
     !is.na(TotalExpenditure),
     !is.na(IncomeComposition),
-    !is.na(Schooling) )
+    !is.na(Schooling) ) |>
+  mutate(
+    #Round specific columns to varying decimal places
+    LifeExpectancy = round(LifeExpectancy, 2),
+    PercentageExpenditure = round(PercentageExpenditure, 4),
+    TotalExpenditure = round(TotalExpenditure, 2),
+    GDP = round(GDP, 4),
+    IncomeComposition = round(IncomeComposition, 3),
+    Schooling = round(Schooling, 1)) |>
+  mutate(
+    #Change the name of South Korea avoiding misunderstanding
+    Country = ifelse(Country == "Republic of Korea",
+                           "South Korea", Country) )|>
+  mutate(
+    #create continent variable and check the continent of each country
+    Continent = countrycode(Country, "country.name", "continent")) %>%
+      ungroup() 
+    cleaned_expectancy}, # error handling to mitigate any cleaning issues
+  error = function(e) { 
+    message("An error occurred during data cleaning: ", e)
+    NULL  # Return NULL if error occurs
+    })
+
   
   
 #### Save data ####
